172c70aa2a25b49b520185558f0d916b0efea96f
wk-chung <1155175832@link.cuhk.edu.hk>
2024-12-22 23:28:06 +0800

    Update on LLMs support

--- examples/multi_agent_graphrag_parser.py
+++ examples/multi_agent_graphrag_parser.py
@@ -15,25 +15,16 @@ import glob
 logging.basicConfig(level=logging.WARNING)
 logging.getLogger("nano-graphrag").setLevel(logging.INFO)
 
-# Assumed llm model settings - Mistral
-llm_api_key_mistral = "WkR7n3wrHUFz02P7NgjweucocW2yIRFZ"
-llm_model_name_mistral = "mistral-large-latest"
-
-# Assumed llm model settings - Deepseek
-llm_base_url_deepseek = os.getenv("LLM_BASE_URL")
-llm_api_key_deepseek = os.getenv("LLM_API_KEY")
-llm_model_name_deepseek = os.getenv("LLM_MODEL")
-
 # Assumed embedding model settings
-embedding_model_name = "nomic-embed-text"
+embedding_model_name = "nomic-embed-text:ctx32k"
 embedding_model_dim = 768
-embedding_model_max_tokens = 8192
+embedding_model_max_tokens = 32000
 
 # Mistral LLM Model
 async def llm_model_mistral_if_cache(
         prompt, system_prompt=None, history_messages=[], **kwargs
 ) -> str:
-    client = Mistral(api_key=llm_api_key_mistral)
+    client = Mistral(api_key=llm_api_key)
     messages = []
     if system_prompt:
     # system_prompt = "You are a answer agent to answer a question."
@@ -52,7 +43,7 @@ async def llm_model_mistral_if_cache(
 
     # print("kwargs",kwargs)
     response = client.chat.complete(
-        model=llm_model_name_mistral, messages=messages, **kwargs
+        model=llm_model, messages=messages, **kwargs
     )
 
     # Cache the response if having-------------------
@@ -64,12 +55,12 @@ async def llm_model_mistral_if_cache(
     return response.choices[0].message.content
 
 
-SYSTEM_PROMPT_TEMPLATE = "You are an intelligent assistant and will follow the instructions given to you to fulfill the goal. The answer should be in the format as in the given example."
+SYSTEM_PROMPT_TEMPLATE = "You are an intelligent assistant and will follow the instructions given to you to fulfill the goal. The answer should be in the format as in the given example. Respond in JSON format."
 async def llm_model_deepseek_if_cache(
         prompt, system_prompt=SYSTEM_PROMPT_TEMPLATE, history_messages=[], **kwargs
 ) -> str:
     openai_async_client = AsyncOpenAI(
-        api_key=llm_api_key_deepseek, base_url=llm_base_url_deepseek
+        api_key=llm_api_key, base_url=llm_base_url
     )
     messages = []
     if system_prompt:
@@ -88,7 +79,7 @@ async def llm_model_deepseek_if_cache(
     # -----------------------------------------------------
 
     response = await openai_async_client.chat.completions.create(
-        model=llm_model_name_deepseek, messages=messages, **kwargs
+        model=llm_model, messages=messages, **kwargs
     )
 
     # Cache the response if having-------------------
@@ -120,47 +111,55 @@ async def ollama_embedding(texts: list[str]) -> np.ndarray:
 
     return embed_text
 
-
-WORKING_DIR = "./nano_graphrag_cache_mistral_test2"
 # Insert
-def insert(File_Directory):
-    from time import time
+# def insert(File_Directory):
+#     from time import time
 
-    remove_if_exist(f"{WORKING_DIR}/vdb_entities.json")
-    remove_if_exist(f"{WORKING_DIR}/kv_store_full_docs.json")
-    remove_if_exist(f"{WORKING_DIR}/kv_store_text_chunks.json")
-    remove_if_exist(f"{WORKING_DIR}/kv_store_community_reports.json")
-    remove_if_exist(f"{WORKING_DIR}/graph_chunk_entity_relation.graphml")
+#     remove_if_exist(f"{WORKING_DIR}/vdb_entities.json")
+#     remove_if_exist(f"{WORKING_DIR}/kv_store_full_docs.json")
+#     remove_if_exist(f"{WORKING_DIR}/kv_store_text_chunks.json")
+#     remove_if_exist(f"{WORKING_DIR}/kv_store_community_reports.json")
+#     remove_if_exist(f"{WORKING_DIR}/graph_chunk_entity_relation.graphml")
 
-    rag = GraphRAG(
-        working_dir=WORKING_DIR,
-        enable_llm_cache=True,
-        best_model_func=llm_model_mistral_if_cache,
-        cheap_model_func=llm_model_mistral_if_cache,
-        embedding_func=ollama_embedding,
-    )
-    start = time()
-    file_list = glob.glob(File_Directory + '/' + 'book.txt')
-    for filename in file_list:
-        with open(filename, 'r', encoding='utf-8-sig') as file:
-            # Read the contents of the file
-            file_contents = file.read()
-            rag.insert(file_contents)
+#     rag = GraphRAG(
+#         working_dir=WORKING_DIR,
+#         enable_llm_cache=True,
+#         best_model_func=llm_model_mistral_if_cache,
+#         cheap_model_func=llm_model_mistral_if_cache,
+#         embedding_func=ollama_embedding,
+#     )
+#     start = time()
+#     file_list = glob.glob(File_Directory + '/' + 'book.txt')
+#     for filename in file_list:
+#         with open(filename, 'r', encoding='utf-8-sig') as file:
+#             # Read the contents of the file
+#             file_contents = file.read()
+#             rag.insert(file_contents)
 
-    print("indexing time:", time() - start)
+#     print("indexing time:", time() - start)
 
 
 # Agent Works Here
-def query_retrieval_strategy(QUERY_QUESTION):
-    client = Mistral(api_key=llm_api_key_mistral)
+def query_retrieval_strategy(QUERY_QUESTION, mistral):
+    if mistral:
+        client = Mistral(api_key=llm_api_key)
+    else:
+        client = AsyncOpenAI(
+            api_key=llm_api_key, base_url=llm_base_url
+        )
     messages = []
-    system_prompt = "You are an intelligent agent responsible for determining the appropriate query type based on the provided scenario. Your query types include local, global, and naive queries. I will give you a question and you need to analyze the question and choose the best query type to obtain the required information efficiently. Please give the answer only."
+    system_prompt = "You are an intelligent agent responsible for determining the appropriate query type based on the provided scenario. Your query types include local, global, and naive queries. I will give you a question and you need to analyze the question and choose the best query type to obtain the required information efficiently. Please give the answer only. Respond in JSON format."
     messages.append({"role": "system", "content": system_prompt})
     messages.append({"role": "user", "content": QUERY_QUESTION})
-    chat_response = client.chat.complete(
-        model=llm_model_name_mistral,
-        messages=messages
-    )
+    if mistral:
+        chat_response = client.chat.complete(
+            model=llm_model,
+            messages=messages
+        )
+    else:
+        chat_response = client.chat.completions.create(
+            model=llm_model, messages=messages
+        )
     print('Running retrieval strategy agent')
     result: Literal["local","global","naive"] = "global"
     response: str = chat_response.choices[0].message.content
@@ -173,17 +172,29 @@ def query_retrieval_strategy(QUERY_QUESTION):
     return result
 
 
-def query_graphrag(QUERY_QUESTION, mode):
-    rag = GraphRAG(
-        working_dir=WORKING_DIR,
-        best_model_func=llm_model_mistral_if_cache,
-        cheap_model_func=llm_model_mistral_if_cache,
-        embedding_func=ollama_embedding,
-    )
-    response = rag.query(
-        QUERY_QUESTION,
-        param=QueryParam(mode=mode)
-    )
+def query_graphrag(QUERY_QUESTION, mode, mistral):
+    if mistral:
+        rag = GraphRAG(
+            working_dir=WORKING_DIR,
+            best_model_func=llm_model_mistral_if_cache,
+            cheap_model_func=llm_model_mistral_if_cache,
+            embedding_func=ollama_embedding,
+        )
+        response = rag.query(
+            QUERY_QUESTION,
+            param=QueryParam(mode=mode)
+        )
+    else:
+        rag = GraphRAG(
+            working_dir=WORKING_DIR,
+            best_model_func=llm_model_deepseek_if_cache,
+            cheap_model_func=llm_model_deepseek_if_cache,
+            embedding_func=ollama_embedding,
+        )
+        response = rag.query(
+            QUERY_QUESTION,
+            param=QueryParam(mode=mode)
+        )
     return response
 
 if __name__ == '__main__':
@@ -216,11 +227,11 @@ if __name__ == '__main__':
     #     help='Using alternate multi-agent workflow (without the use of retrieval_strategy_agent)'
     # )
 
-    # parser.add_argument(
-    #     '--mistral',
-    #     action='store_true',
-    #     help='Using Mistral AI as LLM'
-    # )
+    parser.add_argument(
+        '--mistral',
+        action='store_true',
+        help='Using Mistral AI as LLM'
+    )
 
     # Parse the arguments
     args = parser.parse_args()
@@ -230,10 +241,29 @@ if __name__ == '__main__':
     INPUT_PATH = args.query_input_path
     OUTPUT_PATH = args.query_output_path
 
+    try:
+        llm_base_url = os.getenv("LLM_BASE_URL")
+    except:
+        raise Exception("LLM_BASE_URL environment variable is not set.")
+
+    try:
+        llm_api_key = os.getenv("LLM_API_KEY")
+    except:
+        raise Exception("LLM_API_KEY environment variable is not set.")
+    try:
+        llm_model = os.getenv("LLM_MODEL")
+    except:
+        if args.mistral:
+            llm_model = "mistral-large-latest"
+            print("Model set to Mistral-large-latest")
+        else:
+            llm_model = "deepseek-chat"
+            print("Model set to Deepseek-chat")
+
     with open(INPUT_PATH, 'r') as file:
         QUERY_QUESTION = file.read()
 
-    retrieval_strategy = query_retrieval_strategy(QUERY_QUESTION)
+    retrieval_strategy = query_retrieval_strategy(QUERY_QUESTION, args.mistral)
     print("retrieval_strategy", retrieval_strategy)
 
     score_threshold: float = 4.0
@@ -244,7 +274,7 @@ if __name__ == '__main__':
     while max_score < score_threshold and iteration_cnt < iteration_threshold:
         iteration_cnt += 1
 
-        generated_response = query_graphrag(QUERY_QUESTION, retrieval_strategy)
+        generated_response = query_graphrag(QUERY_QUESTION, retrieval_strategy, args.mistral)
         print("generated_response", generated_response)
 
         FeedBack_Prompt = f"""
@@ -252,7 +282,7 @@ if __name__ == '__main__':
                 Here is the answer: "{generated_response}".
                 Please give your feedback on the answer and give the advantages and disadvantages of the answer"
             """
-        feedback = query_graphrag(FeedBack_Prompt, retrieval_strategy)
+        feedback = query_graphrag(FeedBack_Prompt, retrieval_strategy, args.mistral)
         print("feedback", feedback)
 
         Refinement_Prompt = f"""
@@ -261,15 +291,16 @@ if __name__ == '__main__':
                This is the feedback of the initial response: "{feedback}".
                Please refine the response of the question based on the feedback by highlighting the advantages and addressing the disadvantages mentioned.
             """
-        refined_response = query_graphrag(Refinement_Prompt, retrieval_strategy)
+        refined_response = query_graphrag(Refinement_Prompt, retrieval_strategy, args.mistral)
         print("refined_response", refined_response)
 
         Scoring_Prompt = f"""
                 Here is the question: "{QUERY_QUESTION}".
                 Here is the refined answer: "{refined_response}".
-                Please give a score only from 1-5(type: float) about whether the refined response is satisfactory. Please just give the score only and you must score fairly without worrying about appearances and I am able to tolerate low scores.
+                Please give a score only from 1-5(type: float) in JSON format about whether the refined response is satisfactory. Please just give the score only and you must score fairly without worrying about appearances and I am able to tolerate low scores.
             """
-        score = query_graphrag(Scoring_Prompt, retrieval_strategy)
+        score = query_graphrag(Scoring_Prompt, retrieval_strategy, args.mistral)
+        score = float(score)
         print("score", score)
 
         if score > max_score:
