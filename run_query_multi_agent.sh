6f912d2188fd5a57f3f727be4fdc90aeb490b7b8
SEW Kin Hang <khsew24@cse.cuhk.edu.hk>
2024-12-25 18:14:26 +0800

    Add looporder_response_comparison_async.py

--- run_query_multi_agent.sh
+++ run_query_multi_agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python agents/multi_agent_graphrag.py \
-    --work_directory /research/d2/msc/khsew24/nano-graphrag/nano_graphrag_cache_llm_TEST_multihop \
-    --query_input_path /research/d2/msc/khsew24/nano-graphrag/evaluation/MedQA/input/1.txt \
-    --query_output_path /research/d2/msc/khsew24/nano-graphrag/evaluation/MedQA/output/1.md
+python examples/looporder_response_comparison_async.py \
+    --work_directory nano_graphrag_cache_llm_TEST_web3 \
+    --query_input_path evaluation/web3-smaller/web3_raw_questions \
+    --query_output_path evaluation/web3/responses
